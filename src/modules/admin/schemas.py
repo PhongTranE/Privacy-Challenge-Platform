d7f1e520eeb1d5c4a047710dacc0539a27956745
Phong Trần <phongtranhk20@gmail.com>
2025-05-19 16:28:56 +0700

    feat(invite): enhance InviteKeySchema and add expired key cleanup API

--- src/modules/admin/schemas.py
+++ src/modules/admin/schemas.py
@@ -1,5 +1,8 @@
 from marshmallow import Schema, fields
+from src.constants.app_msg import MISSING_FIELD_ERROR
 
 class InviteKeySchema(Schema):
-    key = fields.Str(dump_only=True)
-    created = fields.DateTime(dump_only=True)
+    key = fields.Str(required=True)
+    created = fields.DateTime(required=True)
+    is_expired = fields.Bool(required=True)
+    creator_id = fields.Int(allow_none=True)
