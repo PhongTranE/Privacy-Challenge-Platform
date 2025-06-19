37fa6dcad83736a15e6ebbce30161f91d0395e42
Phong Trần <phongtranhk20@gmail.com>
2025-06-19 13:14:35 +0700

    Refactor(models): Adjust data models and file handling relationships

--- src/modules/anonymisation/models.py
+++ src/modules/anonymisation/models.py
@@ -30,7 +30,7 @@ class AnonymModel(db.Model):
     )    
     group: so.Mapped["GroupUserModel"] = so.relationship("GroupUserModel", back_populates="anonyms")
     
-    attacks: so.Mapped[list["AttackModel"]] = so.relationship("AttackModel", back_populates="anonym")
+    attacks: so.Mapped[list["AttackModel"]] = so.relationship("AttackModel", back_populates="anonym", cascade="all, delete-orphan")
 
     def __repr__(self):
         return f"<Anonymisation {self.name} - {self.status}>"
